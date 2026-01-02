2a622d99ac590e2d90b6870620a8f81f40af49dc
ashishbasantani <ashishbasantani77@gmail.com>
2026-01-02 09:53:30 +0530

    Added inital setup for app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Welcome to the website of BMC Katni. Site launching soon
+      </p>
+    </>
+  )
+}
+
+export default App
