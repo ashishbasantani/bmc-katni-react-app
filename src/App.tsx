7ab71798310176a53592072644ef9138580cafbc
unknown <aryanmotwani19052002@gmail.com>
2026-01-14 20:58:15 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css';
-import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
 import "./styles/variables.css";
 
 import Header from "./components/Header/Header";
@@ -13,50 +13,44 @@ import Facilities from "./components/Facilities/Facilities";
 import ServiceDetails from "./components/Service/ServiceDetails";
 
 function App() {
+  const location = useLocation();
+
+  // Scroll to section when coming from other pages
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      setTimeout(() => {
+        const el = document.getElementById(id);
+        if (el) el.scrollIntoView({ behavior: "smooth" });
+      }, 200);
+    }
+  }, [location]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
+      <Header />
 
-        <main>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              {/* Home Page */}
-              <Route
-                path="/"
-                element={
-                  <>
-                    <section id="home">
-                      <Hero />
-                    </section>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <section id="home"><Hero /></section>
+                  <section id="facilities"><Facilities /></section>
+                  <section id="about-us"><About /></section>
+                  <section id="services"><Services /></section>
+                  <section id="doctors"><Doctors /></section>
+                </>
+              }
+            />
+            <Route path="/learn-more" element={<ServiceDetails />} />
+          </Routes>
 
-                    <section id="facilities">
-                      <Facilities />
-                    </section>
-
-                    <section id="about-us">
-                      <About />
-                    </section>
-
-                    <section id="services">
-                      <Services />
-                    </section>
-
-                    <section id="doctors">
-                      <Doctors />
-                    </section>
-                  </>
-                }
-              />
-
-              {/* Services Learn More Page */}
-              <Route path="/learn-more" element={<ServiceDetails />} />
-            </Routes>
-
-            <Footer />
-          </Suspense>
-        </main>
-      </BrowserRouter>
+          <Footer />
+        </Suspense>
+      </main>
     </div>
   );
 }
